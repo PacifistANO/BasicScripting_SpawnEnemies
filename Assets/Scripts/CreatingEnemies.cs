3c66664d1e49a6cc0b1597b30d93d404c1770619
PacifistANO <novartiome@yandex.by>
2023-06-08 22:38:20 +0300

    Update CreatingEnemies.cs

--- Assets/Scripts/CreatingEnemies.cs
+++ Assets/Scripts/CreatingEnemies.cs
@@ -9,10 +9,10 @@ using UnityEditor;
 
 public class CreatingEnemies : MonoBehaviour
 {
-    [SerializeField] private Transform _spawn;
+    [SerializeField] private Transform _spawnPoint;
     [SerializeField] private Enemy _enemy;
 
-    private Transform[] _spawns;
+    private Transform[] _spawnPoints;
     private int _currentSpawn = 0;
     private int _countEnemies;
     private float _maxCountEnemies = 6;
@@ -20,11 +20,11 @@ public class CreatingEnemies : MonoBehaviour
 
     private void Start()
     {
-        _spawns = new Transform[_spawn.childCount];
+        _spawnPoints = new Transform[_spawnPoint.childCount];
 
-        for(int i = 0; i < _spawn.childCount; i++)
+        for(int i = 0; i < _spawnPoint.childCount; i++)
         {
-            _spawns[i] = _spawn.GetChild(i);
+            _spawnPoints[i] = _spawnPoint.GetChild(i);
         }
 
         _spawnCoroutine = StartCoroutine(SpawnEnemies());
@@ -36,13 +36,13 @@ public class CreatingEnemies : MonoBehaviour
 
         while (_countEnemies < _maxCountEnemies)
         {
-            if (_currentSpawn < _spawn.childCount)
+            if (_currentSpawn < _spawnPoint.childCount)
             {
-                Instantiate(_enemy, new Vector2(_spawns[_currentSpawn].localPosition.x, _spawns[_currentSpawn].localPosition.y), Quaternion.identity);
+                Instantiate(_enemy, new Vector2(_spawnPoints[_currentSpawn].localPosition.x, _spawnPoints[_currentSpawn].localPosition.y), Quaternion.identity);
                 _currentSpawn++;
                 _countEnemies++;
 
-                if (_currentSpawn == _spawn.childCount)
+                if (_currentSpawn == _spawnPoint.childCount)
                 {
                     _currentSpawn = 0;
                 }
